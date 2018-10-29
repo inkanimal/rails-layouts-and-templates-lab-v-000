298e8d712da9b97befd6b829d5f2a3aa85ddb412
geoffrey woodcock <theatomproject@gmail.com>
2018-10-29 18:14:31 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,6 +1,14 @@
 class StoreAdminController < ApplicationController 
   
-  def admin 
-  end 
+    def home
+    end
+
+    def orders
+        render :layout => "order_administration"
+    end
+
+    def invoice
+        render :layout => false
+    end
   
 end
